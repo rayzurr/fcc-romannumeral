3eef1d98a37c2bcc32837f2112568392172290a6
rayzurr <137191611+rayzurr@users.noreply.github.com>
2024-08-06 13:06:35 +1000

    added script

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Project Title
+
+**Brief description of your project.**
+
+## Getting Started
+
+### Prerequisites
+* List any software or libraries required to run your project.
+
+### Installation
+* Provide instructions on how to set up your project.
+
+### Usage
+* Explain how to use your project.
+
+### Contributing
+* If you're open to contributions, outline guidelines.
+
+### License
+* Specify the license under which your project is released.
+
+## Contact
+* Provide your contact information.
---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("number")
+const convertBtn = document.getElementById("convert-btn")
+const resultMsg = document.getElementById("output")
+
+
+convertBtn.addEventListener("click", (num)=>{
+    let result = '';
+
+    // the for loop with itierate over the key of the roman numerals
+    for (let   
+ key in romanNumerals) {
+
+
+// this check itierate over object and stop when value is greater or equal to the 116
+// a while loop is great for this task because it checks for the first true condition
+        while (num >= romanNumerals[key]) {
+            // will add the key 'C' to the 'result' (which is a string variable)
+            result += key;
+            // originally the value was 116, now it will subtract 100 (the value of C 100) and the continue for the for loop
+            num -= romanNumerals[key];
+        }
+    }
+
+    console.log(result);
+})
+
+
+const romanNumerals = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+};
+
+
+
